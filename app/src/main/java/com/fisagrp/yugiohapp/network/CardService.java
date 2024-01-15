a0c1df59a46cec0a11d39002b61170dc89227567
Gustavo Londa <gustavolonda@gmail.com>
2024-01-15 08:26:26 -0500

    Se agrego agrego el metodo para traiga card por archetype del servidor

--- app/src/main/java/com/fisagrp/yugiohapp/network/CardService.java
+++ app/src/main/java/com/fisagrp/yugiohapp/network/CardService.java
@@ -6,8 +6,12 @@ import com.fisagrp.yugiohapp.model.CardResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface CardService {
     @GET(CARD_INFO)
     Call<CardResponse> getAll();
+
+    @GET(CARD_INFO)
+    Call<CardResponse> getAllByArchetype( @Query("archetype") String archetype);
 }
